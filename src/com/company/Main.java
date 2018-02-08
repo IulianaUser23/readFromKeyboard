08918fc51ecb0df2f1071efdd6f6ddebc08fefa9
IulianaUser23 <fiiuliana@yahoo.ca>
2018-02-08 19:13:59 +0200

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,49 +3,13 @@ package com.company;
 /*If the user pressed number keys( from 0 to 9), the program will tell the number that is pressed, otherwise, program
         will show "Not allowed”. (use a switch case for this)*/
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Introduceti un numar de la 0 la 9: ");
-        int numberFromUser = 0;
-        try {
-            numberFromUser = scan.nextInt();
-        } catch (java.util.InputMismatchException a ) {
-            System.out.println("Nu ati introdus un numar.");;
-        }
 
-        switch (numberFromUser){
-            case 1: numberFromUser=0;
-                System.out.println("Numarul introdus este: " + numberFromUser);
-                break;
-            case 2:numberFromUser=1;
-                System.out.println("Numarul introdus este: " + numberFromUser);
-                break;
-            case 3:numberFromUser=2;
-                System.out.println("Numarul introdus este: " + numberFromUser);
-                break;
-            case 4:numberFromUser=3;
-                System.out.println("Numarul introdus este: " + numberFromUser);
-                break;
-            case 5:numberFromUser=4;
-                System.out.println("Numarul introdus este: " + numberFromUser);
-                break;
-            case 6:numberFromUser=5;
-                System.out.println("Numarul introdus este: " + numberFromUser);
-                break;
-            case 7:numberFromUser=6;
-                System.out.println("Numarul introdus este: " + numberFromUser);
-                break;
-            case 8:numberFromUser=7;
-                System.out.println("Numarul introdus este: " + numberFromUser);
-                break;
-            case 9:numberFromUser=8;
-                System.out.println("Numarul introdus este: " + numberFromUser);
-                break;
-            default: System.out.println("Error!");
-        }
+
+        System.out.println("Introduceti un numar de la 0 la 9: ");
+        ReturnNumber number = new ReturnNumber();
+        number.returnNumber();
     }
 }
---
+++ src/com/company/ReturnNumber.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.util.Scanner;
+
+/**
+ * Created by User on 08.02.2018.
+ */
+public class ReturnNumber {
+
+    public void returnNumber() {
+        Scanner scan = new Scanner(System.in);
+        int numberFromUser = -1;
+
+        try {
+            numberFromUser = scan.nextInt();
+        } catch (java.util.InputMismatchException ss) {
+            System.out.println("Nu ati introdus o cifra.");
+        }
+        switch (numberFromUser) {
+            case 0:
+                System.out.println("Cifra introdusa este: " + numberFromUser);
+                break;
+            case 1:
+                System.out.println("Cifra introdusaeste: " + numberFromUser);
+                break;
+            case 2:
+                System.out.println("Cifra introdusa este: " + numberFromUser);
+                break;
+            case 3:
+                System.out.println("Cifra introdusa este: " + numberFromUser);
+                break;
+            case 4:
+                System.out.println("Cifra introdusa este: " + numberFromUser);
+                break;
+            case 5:
+                System.out.println("Cifra introdusa este: " + numberFromUser);
+                break;
+            case 6:
+                System.out.println("Cifra introdusa este: " + numberFromUser);
+                break;
+            case 7:
+                System.out.println("Cifra introdusa este: " + numberFromUser);
+                break;
+            case 8:
+                System.out.println("Cifra introdusa este: " + numberFromUser);
+                break;
+            case 9:
+                System.out.println("Cifra introdusa este: " + numberFromUser);
+                break;
+            default:
+                System.out.println("Eroare - valoare in afara itervalului 0-9 ");
+        }
+    }
+}
+
